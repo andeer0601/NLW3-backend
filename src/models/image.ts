4a8e2e07f6a66fc917fa8185af208dbb4b4310d9
Anderson <48699535+andeer0601@users.noreply.github.com>
2020-10-13 22:57:04 -0300

    First - Upload Images

---
+++ src/models/image.ts
@@ -0,0 +1,16 @@
+﻿import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm'
+import orphanage from './orphanage'
+
+@Entity('images')
+export default class Image {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column()
+    path: string;
+
+    @ManyToOne(() => orphanage, orphanage => orphanage.images)
+    @JoinColumn({ name: 'orphanage_id' })
+    orphanage: orphanage;
+
+}
